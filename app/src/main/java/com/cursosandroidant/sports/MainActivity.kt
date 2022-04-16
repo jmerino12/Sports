33f49d54ada2a10418c474395affe7afca77bfe5
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-04-16 15:52:54 -0500

    Calculo de fibonacci sin corrutinas

--- app/src/main/java/com/cursosandroidant/sports/MainActivity.kt
+++ app/src/main/java/com/cursosandroidant/sports/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cursosandroidant.sports
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cursosandroidant.sports.databinding.ActivityMainBinding
@@ -28,7 +29,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setupTextField()
         setupRecyclerView()
         setupActionBar()
 
@@ -46,21 +47,53 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         }
     }
 
-    private fun sports(): MutableList<Sport>{
-        val soccerSport = Sport(1, "Fútbol Soccer", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Football_%28Soccer%29.JPG/1024px-Football_%28Soccer%29.JPG")
-        val baseballSport = Sport(2, "Baseball", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/A_worn-out_baseball.JPG/2470px-A_worn-out_baseball.JPG")
-        val volleyballSport = Sport(3, "Volleyball", "https://upload.wikimedia.org/wikipedia/commons/b/bf/Algeria_and_Japan_women%27s_national_volleyball_team_at_the_2012_Summer_Olympics_%287913959028%29.jpg")
-        val boxingSport = Sport(4, "Boxeo", "https://upload.wikimedia.org/wikipedia/commons/4/4d/Boxing_at_the_2018_Summer_Youth_Olympics_%E2%80%93_Girls%27_flyweight_Bronze_Medal_Bout_068.jpg")
-        val tennisSport = Sport(5, "Tenis", "https://upload.wikimedia.org/wikipedia/commons/3/3e/Tennis_Racket_and_Balls.jpg")
-        val rugbySport = Sport(6, "Rugby", "https://upload.wikimedia.org/wikipedia/commons/6/6a/New_Zealand_national_rugby_20191101d4.jpg")
-        val hokeySport = Sport(7, "Hokey", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/2020-01-21_Ice_hockey_at_the_2020_Winter_Youth_Olympics_%E2%80%93_Women%27s_tournament_%E2%80%93_Gold_Medal_Game_%28Martin_Rulsch%29_068.jpg/2560px-2020-01-21_Ice_hockey_at_the_2020_Winter_Youth_Olympics_%E2%80%93_Women%27s_tournament_%E2%80%93_Gold_Medal_Game_%28Martin_Rulsch%29_068.jpg")
-        val golfSport = Sport(8, "Golf", "https://upload.wikimedia.org/wikipedia/commons/d/d9/Golf_ball_2.jpg")
-        val chessSport = Sport(9, "Chess", "https://upload.wikimedia.org/wikipedia/commons/8/8c/Chess_Large.JPG")
-        return mutableListOf(soccerSport, baseballSport, volleyballSport, boxingSport, tennisSport,
-            rugbySport, hokeySport, golfSport, chessSport)
+    private fun sports(): MutableList<Sport> {
+        val soccerSport = Sport(
+            1,
+            "Fútbol Soccer",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Football_%28Soccer%29.JPG/1024px-Football_%28Soccer%29.JPG"
+        )
+        val baseballSport = Sport(
+            2,
+            "Baseball",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/A_worn-out_baseball.JPG/2470px-A_worn-out_baseball.JPG"
+        )
+        val volleyballSport = Sport(
+            3,
+            "Volleyball",
+            "https://upload.wikimedia.org/wikipedia/commons/b/bf/Algeria_and_Japan_women%27s_national_volleyball_team_at_the_2012_Summer_Olympics_%287913959028%29.jpg"
+        )
+        val boxingSport = Sport(
+            4,
+            "Boxeo",
+            "https://upload.wikimedia.org/wikipedia/commons/4/4d/Boxing_at_the_2018_Summer_Youth_Olympics_%E2%80%93_Girls%27_flyweight_Bronze_Medal_Bout_068.jpg"
+        )
+        val tennisSport = Sport(
+            5,
+            "Tenis",
+            "https://upload.wikimedia.org/wikipedia/commons/3/3e/Tennis_Racket_and_Balls.jpg"
+        )
+        val rugbySport = Sport(
+            6,
+            "Rugby",
+            "https://upload.wikimedia.org/wikipedia/commons/6/6a/New_Zealand_national_rugby_20191101d4.jpg"
+        )
+        val hokeySport = Sport(
+            7,
+            "Hokey",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/2020-01-21_Ice_hockey_at_the_2020_Winter_Youth_Olympics_%E2%80%93_Women%27s_tournament_%E2%80%93_Gold_Medal_Game_%28Martin_Rulsch%29_068.jpg/2560px-2020-01-21_Ice_hockey_at_the_2020_Winter_Youth_Olympics_%E2%80%93_Women%27s_tournament_%E2%80%93_Gold_Medal_Game_%28Martin_Rulsch%29_068.jpg"
+        )
+        val golfSport =
+            Sport(8, "Golf", "https://upload.wikimedia.org/wikipedia/commons/d/d9/Golf_ball_2.jpg")
+        val chessSport =
+            Sport(9, "Chess", "https://upload.wikimedia.org/wikipedia/commons/8/8c/Chess_Large.JPG")
+        return mutableListOf(
+            soccerSport, baseballSport, volleyballSport, boxingSport, tennisSport,
+            rugbySport, hokeySport, golfSport, chessSport
+        )
     }
 
-    private fun getAllSports(){
+    private fun getAllSports() {
         val sportsData = sports()
         listAdapter.submitList(sportsData)
         /*sportsData.forEach { sport ->
@@ -77,7 +110,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         }
     }
 
-    private suspend fun getWeather(): WeatherEntity = withContext(Dispatchers.IO){
+    private suspend fun getWeather(): WeatherEntity = withContext(Dispatchers.IO) {
         setupTitle(getString(R.string.main_retrofit_in_progress))
 
         val retrofit: Retrofit = Retrofit.Builder()
@@ -90,7 +123,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         service.getWeatherById(5095808L, "metric", "4621d3c0b8e15ed89d674ded864db076")
     }
 
-    private fun formatResponse(weatherEntity: WeatherEntity){
+    private fun formatResponse(weatherEntity: WeatherEntity) {
         val temp = "${weatherEntity.main.temp} Cº"
         val name = weatherEntity.name
         val country = weatherEntity.sys.country
@@ -106,5 +139,23 @@ class MainActivity : AppCompatActivity(), OnClickListener {
      * */
     override fun onClick(sport: Sport) {
         Snackbar.make(binding.root, sport.name, Snackbar.LENGTH_SHORT).show()
+    }
+
+    private fun setupTextField() {
+        binding.etNumber.addTextChangedListener {
+            binding.tvResult.text = "Calculando...."
+            val time = System.currentTimeMillis()
+
+            if (it.toString().isNotEmpty()) {
+                val fib = fibonacci(it.toString().toLong())
+                binding.tvResult.text =
+                    "R = ${"%,d".format(fib)}\n (in ${System.currentTimeMillis() - time}) ms"
+            }
+        }
+    }
+
+    private fun fibonacci(n: Long): Long {
+        return if (n <= 1) n
+        else fibonacci(n - 1) + fibonacci(n - 2)
     }
 }
